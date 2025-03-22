899d09dc4891dacdafcbac7e26616d7f0f4bfcc0
Lucas Gama <lucasgdon@gmail.com>
2025-03-22 15:56:40 -0300

    Interface de evento criada

---
+++ src/Domain/Event/EventRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Domain\Event;
+
+interface EventRepositoryInterface
+{
+    public function findAll(): array;
+    public function find(int $id): ?Event;
+    public function save(Event $event): ?int;
+    public function update(Event $event): bool;
+    public function delete(int $id): bool;
+}
